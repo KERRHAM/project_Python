c23fd4467af024b32974fa19ead4fb436c8dd27b
Kerr Hamilton <kerr9928@gmail.com>
2024-08-01 19:20:39 +0000

    Add function display vending machine

--- run.py
+++ run.py
@@ -64,3 +64,16 @@ def spin_vending_machine(rows, cols, symbols):
         columns.append(column)
 
     return columns
+
+# Function for displaying game board
+# Empty print() used to start newline after board is displayed for vertical spacing
+def display_vending_machine(columns):
+
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        
+        print()
